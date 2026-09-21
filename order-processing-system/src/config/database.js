0bd48cd5a1c11d238ac2d332648992aff6359399
Protiviti <141806559+protiviti-yogesh@users.noreply.github.com>
2026-09-21 14:08:04 +0530

    addec config

--- order-processing-system/src/config/database.js
+++ order-processing-system/src/config/database.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: "localhost",
+    host: "order-processing-db.chym24c2q5hm.ap-south-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "root",
-    database: "orders_db",
+    user: "yogeshsaini",
+    password: "Ejoty157",
+    database: "ordersDbyogi",
 
     waitForConnections: true,
     connectionLimit: 10,
